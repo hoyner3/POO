ba3454da9b0520b47670febb60d85a87794aa287
unknown <hoyner26@gmail.com>
2023-03-11 18:31:47 -0600

    Class Contacto

--- semana 5/Trabajo Contacto/proyecto/src/App.java
+++ semana 5/Trabajo Contacto/proyecto/src/App.java
@@ -1,5 +1,17 @@
+import java.util.Date;
+
+
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+    public static void main(String[] args){
+
+        
+        Agenda agenda = new Agenda();//Instanciamos la clase Agenda
+        Persona persona1 = new Persona("Juan", "Perez", "Calle 1");//Instanciamos la clase Persona
+        Contacto contacto1 = new Familia(persona1, "Familia", 12112121, "hoyner@gmail.com", "Hermano", new Date(2020, 12, 12););//Instanciamos la clase Contacto
+        //dame un contacto de tipo familia 
+        
+        System.out.println(agenda);
+
+        
 }
